2d3e53116a7028c281c845c321b3abf06da7eba9
BART! <bart@bartweb.cz>
2015-09-13 12:34:15 +0200

    Add choice: Completely enable/disable plugin
    System settings *active*

--- core/components/queeg/elements/plugins/queeg.plugin.php
+++ core/components/queeg/elements/plugins/queeg.plugin.php
@@ -1,88 +1,90 @@
 <?php
-switch ($modx->event->name) {
-    case 'OnWebPagePrerender':
+if ($modx->getOption('queeg.active', null, true)) {
+    switch ($modx->event->name) {
+        case 'OnWebPagePrerender':
 
-        if ($modx->context->get('key') === "mgr") {
-            break;
-        }
-
-        if ($modx->user->hasSessionContext('mgr')) {
-            $appname =  'queeg';
-            $contentArray = array();
-            $systemArray = array();
-
-            // get queeg version
-            $package = $modx->getObject('transport.modTransportPackage', array('package_name' => 'queeg'));
-            if ($package) {
-                $major = $package->get('version_major');
+            if ($modx->context->get('key') === "mgr") {
+                break;
             }
 
-            // get user's language
-            $userSettings = $modx->user->getSettings();
-            $lang = $userSettings['manager_language'];
+            if ($modx->user->hasSessionContext('mgr')) {
+                $appname =  'queeg';
+                $contentArray = array();
+                $systemArray = array();
 
-            if (!$lang) {
-                $lang = $modx->getOption('manager_language', null, 'en');
-            }
+                // get queeg version
+                $package = $modx->getObject('transport.modTransportPackage', array('package_name' => 'queeg'));
+                if ($package) {
+                    $major = $package->get('version_major');
+                }
 
-            $modx->setOption('cultureKey', $lang);
-            $modx->lexicon->load('core:default');
+                // get user's language
+                $userSettings = $modx->user->getSettings();
+                $lang = $userSettings['manager_language'];
 
-            // Initialize system settings
-            $api =  $major;
-            $param['id'] =  true;
-            $param['published'] = (boolean) $modx->getOption('queeg.published', null, true);
-            $param['editedon'] = (boolean) $modx->getOption('queeg.editedon', null, true);
-            $param['editedby'] = (boolean) $modx->getOption('queeg.editedby', null, true);
-            $custom_fields = $modx->getOption('queeg.custom_fields', null, false);
-            $date_format = $modx->getOption('queeg.date_format', null, 'Y-m-d H:i');
+                if (!$lang) {
+                    $lang = $modx->getOption('manager_language', null, 'en');
+                }
 
-            // Define system fields
-            $systemFields = array('id', 'published');
+                $modx->setOption('cultureKey', $lang);
+                $modx->lexicon->load('core:default');
 
-            if ($custom_fields) {
-                $cFields = array_map('trim', explode(",", $custom_fields)); // explode and trim
-                $cArray = array_fill_keys($cFields, true); // Change val => key and set true
-                $param = array_merge($param, $cArray); // merge default fields and custom fields
-            }
+                // Initialize system settings
+                $api =  $major;
+                $param['id'] =  true;
+                $param['published'] = (boolean) $modx->getOption('queeg.published', null, true);
+                $param['editedon'] = (boolean) $modx->getOption('queeg.editedon', null, true);
+                $param['editedby'] = (boolean) $modx->getOption('queeg.editedby', null, true);
+                $custom_fields = $modx->getOption('queeg.custom_fields', null, false);
+                $date_format = $modx->getOption('queeg.date_format', null, 'Y-m-d H:i');
 
-            $params = array_filter($param); // remove false options
+                // Define system fields
+                $systemFields = array('id', 'published');
 
-            foreach ($params as $key => $value) {
+                if ($custom_fields) {
+                    $cFields = array_map('trim', explode(",", $custom_fields)); // explode and trim
+                    $cArray = array_fill_keys($cFields, true); // Change val => key and set true
+                    $param = array_merge($param, $cArray); // merge default fields and custom fields
+                }
 
-                $data = $modx->resource->$key; // get each placeholder from resource (id, published, ...)
+                $params = array_filter($param); // remove false options
 
-                if ($key == 'editedby') {
-                    if ($data) {
-                        $data = $modx->getObject('modUser', $data)->get('username');
+                foreach ($params as $key => $value) {
+
+                    $data = $modx->resource->$key; // get each placeholder from resource (id, published, ...)
+
+                    if ($key == 'editedby') {
+                        if ($data) {
+                            $data = $modx->getObject('modUser', $data)->get('username');
+                        }
                     }
+
+                    if ($key == 'editedon') {
+                        $data = date($date_format, $data);
+                    }
+
+                    // System Values
+                    if (in_array($key, $systemFields)) {
+                        $systemArray[$key] = $data;
+                    }
+
+                    // Content values
+                    $key = str_replace("'", '&apos;', $modx->lexicon($key));
+                    $contentArray[$key] = $data;
                 }
 
-                if ($key == 'editedon') {
-                    $data = date($date_format, $data);
-                }
+                $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
 
-                // System Values
-                if (in_array($key, $systemFields)) {
-                    $systemArray[$key] = $data;
-                }
+                // Add system params
+                $systemArray['host'] = $protocol . $_SERVER['HTTP_HOST'];
+                $systemArray['manager'] = MODX_MANAGER_URL;
 
-                // Content values
-                $key = str_replace("'", '&apos;', $modx->lexicon($key));
-                $contentArray[$key] = $data;
+                $system = json_encode($systemArray);
+                $content = json_encode($contentArray);
+
+                $output = &$modx->resource->_output;
+                $output = preg_replace('/(<\/head>(?:<\/head>)?)/i',"<meta name='{$appname}' content='{$content}' data-system='{$system}' data-api='{$api}' />\r\n$1", $output);
             }
-
-            $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
-
-            // Add system params
-            $systemArray['host'] = $protocol . $_SERVER['HTTP_HOST'];
-            $systemArray['manager'] = MODX_MANAGER_URL;
-
-            $system = json_encode($systemArray);
-            $content = json_encode($contentArray);
-
-            $output = &$modx->resource->_output;
-            $output = preg_replace('/(<\/head>(?:<\/head>)?)/i',"<meta name='{$appname}' content='{$content}' data-system='{$system}' data-api='{$api}' />\r\n$1", $output);
-        }
-        break;
+            break;
+    }
 }
--- core/components/queeg/lexicon/en/default.inc.php
+++ core/components/queeg/lexicon/en/default.inc.php
@@ -10,9 +10,11 @@ $_lang['setting_queeg.editedon_desc'] = 'Show "Edited on" output';
 $_lang['setting_queeg.editedby'] = 'Edited by';
 $_lang['setting_queeg.editedby_desc'] = 'Show "published by" output';
 
-
 $_lang['setting_queeg.date_format'] = 'Date Format';
 $_lang['setting_queeg.date_format_desc'] = 'The format string, in PHP date() format, for the dates represented in the manager.';
 
 $_lang['setting_queeg.custom_fields'] = 'Custom Fields (comma separated)';
 $_lang['setting_queeg.custom_fields_desc'] = 'You can define comma separated list of additional fields (e.g.: pagetitle, alias)';
+
+$_lang['setting_queeg.active'] = 'Queeg Active';
+$_lang['setting_queeg.active_desc'] = 'Enable or completely disable Queeg';
